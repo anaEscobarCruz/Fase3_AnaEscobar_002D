88fa613b2b27228a66c8e6f938c036028e0dd70d
anaEscobarCruz <anl.escobar@alumnos.duoc.cl>
2020-11-30 05:38:10 -0300

    Requerimientos Admin

--- articulos/catalogo/test/test_models.py
+++ articulos/catalogo/test/test_models.py
@@ -3,9 +3,9 @@ from articulos.catalogo.models import Producto1
 
 class Producto1TestCase(TestCase):
     def setUp(self):
-        Producto1.objects.create(id="977",nom="Tazón")
+        Producto1.objects.create(id="997",nom="Tazón")
 
     def test_Producto1(self):
-        producto = Producto1.objects.get(id="977")
+        producto = Producto1.objects.get(id="997")
         self.assertEqual(producto.nom, "Tazón")
     
